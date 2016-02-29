254da3ae9bfb3e78045e8d69248dd24a20aece57
Tevin Meredith <tevin.meredith@gmail.com>
2016-02-28 20:15:48 -0500

    Update to Guard Script
    Made a prototype to allow for a longer and more accurate patrol.

--- CatBurglars_ProjectFolder/Assets/Scripts/TestGuardWalk.cs
+++ CatBurglars_ProjectFolder/Assets/Scripts/TestGuardWalk.cs
@@ -5,6 +5,8 @@ public class TestGuardWalk : MonoBehaviour {
 
     public Transform destination;
     public Transform destination2;
+    public Transform[] patrolDestinations;
+    public int patrolCount = 0;
 
     private NavMeshAgent agent;
     private bool reached = false;
@@ -22,31 +24,48 @@ public class TestGuardWalk : MonoBehaviour {
 
     void OnTriggerEnter(Collider col)
     {
-        if(col.transform.gameObject.tag == "Path1")
+        if(col.transform.gameObject.tag == "Guard Path")
         {
-            Debug.Log("Reached has happened");
-            reached = true;
+            Debug.Log("Count --" + patrolCount);
+            //reached = true;
             StartCoroutine(NewChase());
         }
-        if(col.transform.gameObject.tag == "Path2")
+        else if(col.transform.gameObject.tag == "Guard Path")
         {
-            reached = false;
+            //reached = false;
             StartCoroutine(NewChase());
         }
     }
     
     IEnumerator NewChase()
     {
-        if (reached == false)
+        /*if (reached == false)
         {
             yield return new WaitForSeconds(1f);
-            agent.SetDestination(destination.position);
+            //agent.SetDestination(destination.position);
+            agent.SetDestination(patrolDestinations[patrolCount].position);
         }
         else {
             Debug.Log("Let's do this shit");
             yield return new WaitForSeconds(1f);
-            agent.SetDestination(destination2.position);
+            //agent.SetDestination(destination2.position);
+            agent.SetDestination(patrolDestinations[patrolCount].position);
             reached = false;
+        } */
+
+        if(patrolCount == 1)
+        {
+            patrolCount--;
+            Debug.Log(patrolCount);
+            yield return new WaitForSeconds(1f);
+            agent.SetDestination(patrolDestinations[patrolCount].position);
+        }
+        else
+        {
+            patrolCount++;
+            Debug.Log(patrolCount);
+            yield return new WaitForSeconds(1f);
+            agent.SetDestination(patrolDestinations[patrolCount].position);
         }
     }
 }
